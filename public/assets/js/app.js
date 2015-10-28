ad9c2020d807c6cfc5a6648871e86b7e897ce063
liangsu1210 <su@c14creative.com>
2015-10-28 23:04:20 +0000

    navi finished

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -25,24 +25,22 @@ hshs.controller('indexCtrl', function($scope) {
     desc: '设计师平台'
   };
   $scope.swiper = {};
-         $scope.next = function(){
-           $scope.swiper.slideNext();
-         };
-
-        $scope.onReadySwiper = function (swiper) {
-          
-          swiper.on('slideChangeStart', function () {
-          
-            console.log('slideChangeStart');
-          });
-        };
+   $scope.next = function(){
+     $scope.swiper.slideNext();
+   };
+  $scope.onReadySwiper = function (swiper) {
+    swiper.on('slideChangeStart', function () {
+      console.log('slideChangeStart');
+    });
+  };
 });
 
 hshs.controller('productsCtrl', function($scope) {
-  var categories, designers, i, prices, product, products,texts;
+  var categories, designers, i, prices, product, products,texts,priceranges;
   designers = ['Alexander McQueen', 'Acne Studios', 'Cedric Jacquemyn', 'Christian Louboutin', 'Christopher Kane', 'Ann Demeulemeester', 'Alexander Wang', 'Chi Zhang'];
   categories = ['裙子', '大衣', '针织衫', '夹克', '内衣', '裤子', '衬衣', '西服', 'T恤', '泳装', '其它'];
-  prices = ['￥1000以下', '￥1000-￥2000', '￥2000-￥5000', '￥5000-￥10000', '￥10000以上'];
+  priceranges = ['￥1000以下', '￥1000-￥2000', '￥2000-￥5000', '￥5000-￥10000', '￥10000以上'];
+  prices = ['34', '23', '123', '54', '56'];
   texts = ['Phillip Lim是华裔美籍设计师Phillip Lim和他的生意伙伴Wen Zhou于2005年创立的品牌，品牌服装凭低调但不失奢华感的设计荣获各大时装奖项之余，更从中取得良好的口碑。'];
   products = new Array();
   i = 0;
@@ -51,7 +49,8 @@ hshs.controller('productsCtrl', function($scope) {
       id: i,
       designer: designers[i % 8],
       category: categories[i % 11],
-      price: Math.floor(Math.random() * 20000),
+      pricerange: Math.floor(Math.random() * 20000),
+      price: prices[i % 8],
       text:texts,
     };
     products.push(product);
@@ -60,10 +59,13 @@ hshs.controller('productsCtrl', function($scope) {
   $scope.designers = designers;
   $scope.categories = categories;
   $scope.prices = prices;
+  $scope.priceranges = priceranges;
   $scope.products = products;
   $scope.texts = texts;
   $scope.maxSize = 6;
   $scope.fourItem = 4;
+  $scope.threeItem = 3;
+  $scope.oneItem = 1;
   $scope.bigTotalItems = 175;
   return $scope.bigCurrentPage = 1;
 });
